5cb1abb9a0e07b78dc784a910ff6a089070c9143
Roknarok <afrik.gemenis@gmail.com>
2021-05-13 14:08:38 +0300

    End of Section3

--- src/App/Layout/App.jsx
+++ src/App/Layout/App.jsx
@@ -1,11 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../Features/event/EventDashboard/EventDashboard";
+import NavBar from "../../Features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Re-vents</h1>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className='main'>
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
